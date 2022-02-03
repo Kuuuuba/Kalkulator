511f436d5dad1386cb5af936035e3147f21baccd
Kuuuuba <96994137+Kuuuuba@users.noreply.github.com>
2022-02-03 17:37:28 +0100

    Add files via upload

---
+++ kalkulator.py
@@ -0,0 +1,7 @@
+a = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
+b = int(input("Podaj składnik 1"))
+c = int(input("Podaj składnik 2"))
+operacja = input("+,-,*,/")
+print(operacja)
+if (operacja == "+"):
+    print("Wynik to", b+c)
